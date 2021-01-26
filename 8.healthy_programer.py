82ab8707c9349329b514dea6381732c989c6928d
Md-Abid <78022177+Md-Abid@users.noreply.github.com>
2021-01-26 19:11:43 +0600

    Add files via upload

---
+++ 8.healthy_programer.py
@@ -0,0 +1,50 @@
+from pygame import mixer
+from datetime import datetime
+from time import time
+
+def musiconloop(file, stopper):
+    mixer.init()
+    mixer.music.load(file)
+    mixer.music.play()
+    while True:
+        input_of_user = input()
+        if input_of_user == stopper:
+            mixer.music.stop()
+            break
+
+def log_now(msg):
+    with open("mylogs.txt", "a") as f:
+        f.write(f"{msg} {datetime.now()}\n")
+
+name=input("Enter your name: ")
+name=name.upper()
+print(f"Hello {name}! Welcome to healthy programmer.")
+print("This will help you to fit your body and mind.")
+
+if __name__ == '__main__':
+    # musiconloop("water.mp3", "stop")
+    init_water = time()
+    init_eyes = time()
+    init_exercise = time()
+    watersecs = 40*60
+    exsecs = 30*60
+    eyessecs = 45*60
+
+    while True:
+        if time() - init_water > watersecs:
+            print(f"Hello {name}!It is time to drink some water. Enter 'drank' to stop the alarm.")
+            musiconloop('water.mp3', 'drank')
+            init_water = time()
+            log_now("Drank Water at")
+
+        if time() - init_eyes >eyessecs:
+            print(f"Hello {name}!It is time to relaxed your eyes for 60 second. Enter 'doneeyes' to stop the alarm.")
+            musiconloop('eyes.mp3', 'doneeyes')
+            init_eyes = time()
+            log_now("Eyes Relaxed at")
+
+        if time() - init_exercise > exsecs:
+            print(f"Hello {name}!It is time to do some physical exercise. Enter 'donephy' to stop the alarm.")
+            musiconloop('physical.mp3', 'donephy')
+            init_exercise = time()
+            log_now("Physical Activity done at")
